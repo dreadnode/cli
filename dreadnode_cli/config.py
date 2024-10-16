10f6d3e59c31c2ad22bfb628b3d31c43b10926f5
evilsocket <evilsocket@gmail.com>
2024-10-16 14:09:59 +0200

    docs: more code comments

--- dreadnode_cli/config.py
+++ dreadnode_cli/config.py
@@ -5,21 +5,32 @@ from dreadnode_cli.defaults import USER_CONFIG_PATH
 
 
 class ServerConfig(pydantic.BaseModel):
+    """Server specific authentication data and API URL."""
+
     url: str
     access_token: str
     refresh_token: str
 
 
 class UserConfig(pydantic.BaseModel):
+    """User configuration supporting multiple server profiles."""
+
     servers: dict[str, ServerConfig] = {}
-    active: str | None = None
+    active: str = ""
 
     def _update_active(self) -> None:
+        """If active is not set, set it to the first available server and raise an error if no servers are configured."""
+
         if self.active not in self.servers:
-            self.active = next(iter(self.servers)) if self.servers else None
+            self.active = next(iter(self.servers)) if self.servers else ""
+
+        if not self.active or not self.servers:
+            raise Exception("no servers are configured, use [bold]dreadnode login[/] to authenticate")
 
     @classmethod
     def read(cls) -> "UserConfig":
+        """Read the user configuration from the file system or return an empty instance."""
+
         if not USER_CONFIG_PATH.exists():
             return cls()
 
@@ -27,39 +38,48 @@ class UserConfig(pydantic.BaseModel):
             return cls.model_validate_json(f.read())
 
     def write(self) -> None:
+        """Write the user configuration to the file system."""
+
         self._update_active()
 
         if not USER_CONFIG_PATH.parent.exists():
-            print(f":rocket: Creating user configuration directory: {USER_CONFIG_PATH.parent}")
+            print(f":rocket: creating user configuration directory: {USER_CONFIG_PATH.parent}")
             USER_CONFIG_PATH.parent.mkdir(parents=True)
 
         with USER_CONFIG_PATH.open("w") as f:
             f.write(self.model_dump_json(indent=2))
 
     def get_profile_config(self, profile: str) -> ServerConfig | None:
+        """Get the server configuration for the given profile or None if not set."""
+
         return self.servers.get(profile)
 
     def set_profile_config(self, profile: str, config: ServerConfig) -> "UserConfig":
+        """Set the server configuration for the given profile."""
+
         self.servers[profile] = config
         return self
 
     def set_active_config(self, config: ServerConfig) -> "UserConfig":
-        self._update_active()
-        if not self.active or not self.servers:
-            raise Exception("No servers are configured, use [bold]dreadnode login[/] to authenticate")
+        """Set the active server configuration."""
 
+        self._update_active()
         self.servers[self.active] = config
         return self
 
     def remove_profile_config(self, profile: str) -> "UserConfig":
+        """Remove the server configuration for the given profile."""
+
         if profile in self.servers:
             del self.servers[profile]
         return self
 
     @property
     def active_server(self) -> ServerConfig:
+        """Get the active server configuration."""
+
         self._update_active()
         if not self.active or not self.servers:
-            raise Exception("No servers are configured, use [bold]dreadnode login[/] to authenticate")
+            raise Exception("no servers are configured, use [bold]dreadnode login[/] to authenticate")
 
         return self.servers[self.active]
--- dreadnode_cli/defaults.py
+++ dreadnode_cli/defaults.py
@@ -1,14 +1,20 @@
 import os
 import pathlib
 
+# name of the default server profile
 MAIN_PROFILE_NAME = "main"
+# default server URL
 PLATFORM_BASE_URL = "https://crucible.dreadnode.io"
 
+# path to the user configuration file
 USER_CONFIG_PATH = pathlib.Path(
+    # allow overriding the user config file via env variable
     os.getenv("DREADNODE_USER_CONFIG_FILE") or pathlib.Path.home() / ".dreadnode" / "config.json"
 )
 
+# default poll interval for the authentication flow
 DEFAULT_POLL_INTERVAL = 5
+# default maximum poll time for the authentication flow
 DEFAULT_MAX_POLL_TIME = 300
-
+# default maximum token TTL in seconds
 DEFAULT_TOKEN_MAX_TTL = 60
